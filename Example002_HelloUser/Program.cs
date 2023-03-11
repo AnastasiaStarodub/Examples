5b6edab1a93350e9648330b434cff0299d22e204
Anastasia <anastasiastarodub@gmail.com>
2023-03-11 11:44:58 +0300

    Exmple002 Hello User

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,8 +1,6 @@
-﻿Console.Write ("Введите ваше имя ");
-string username = Console.ReadLine ();
-Console.Write ("Привет, ");
-Console.Write (username);
-
-
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
 
 
